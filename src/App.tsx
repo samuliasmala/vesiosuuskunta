a10f237376d5461e2ed87fab4329973a653d6596
Samuli Asmala <samuli.asmala@gmail.com>
2022-02-28 14:39:12 +0200

    Add water price calculation form

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,92 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="container">
+        <h1>Pinsiön alueen vesiosuuskunnan vesilaskuri</h1>
+
+        <Field name="prevAmount" text="Edellinen lukema: " type="number">
+          m<sup>2</sup>
+        </Field>
+
+        <Field name="currAmount" text="Nykyinen lukema: " type="number">
+          m<sup>2</sup>
+        </Field>
+
+        <Field name="usage" text="Kulutus: " type="number" disabled>
+          m<sup>2</sup>
+        </Field>
+
+        <Field name="unitPrice" text="Veden yksikköhinta: " type="number" value="1.5" disabled>
+          €/m<sup>2</sup>
+        </Field>
+
+        <Field name="waterPrice" text="Veden hinta: " type="number" disabled>
+          €
+        </Field>
+
+        <Field name="baseFee" text="Perusmaksu: " type="number" value="50" disabled>
+          €
+        </Field>
+
+        <Field name="totalPrice" text="Yhteensä: " type="number" disabled>
+          €
+        </Field>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+const Field: React.FC<{
+  name: string;
+  text: string;
+  type: string;
+  value?: string;
+  disabled?: boolean;
+}> = ({ children, name, text, type, value, disabled }) => {
+  return (
+    <div className={`row formRow ${name}Row`}>
+      <Label name={name} text={text} />
+      <Value type={type} name={name} value={value} disabled={disabled} />
+      <Unit>{children}</Unit>
+    </div>
+  );
+};
+
+const Label: React.FC<{ name: string; text: string }> = ({ name, text }) => {
+  return (
+    <div className="col labelCol">
+      <label htmlFor={name}>{text}</label>
+    </div>
+  );
+};
+
+const Value: React.FC<{ type: string; name: string; value?: string; disabled?: boolean }> = ({
+  type,
+  name,
+  value,
+  disabled,
+}) => {
+  return (
+    <div className="col valueCol">
+      <input
+        type={type}
+        name={name}
+        value={value}
+        disabled={disabled}
+        className={`inputField ${name}Field`}
+      />
+    </div>
+  );
+};
+
+const Unit: React.FC = ({ children }) => {
+  return <div className="col unitCol">{children}</div>;
+};
+
+export default App;
